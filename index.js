8c45d869e240925987b605cdf99fea01e5742fcc
hndryno <hendriyono97@gmail.com>
2019-09-23 17:13:53 +0700

    berhasil menambahkan data

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const test = 'berhasil'
-console.log(test)
+const express = require('express')
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    var x, y, z
+    x = 5
+    y = 12
+    z = x + y
+
+    return res.send(`The value of z is ${z}`)
+})
+
+app.post('/', (req, res) => {
+    let email = req.body.email
+    
+    return res.send(`The email value is ${email}`)
+})
+
+app.listen(3300, () => {
+    console.log(`Example app listening on port 3300`)
+})
